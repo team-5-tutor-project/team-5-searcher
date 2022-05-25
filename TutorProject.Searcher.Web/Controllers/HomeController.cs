f1c325d0cebe4f50b7a5d8ec22ca153c164b11d8
teterina_m_o <teterina_m_o@rambler.ru>
2022-05-25 21:35:56 +0300

    database configured

---
+++ TutorProject.Searcher.Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TutorProject.Account.Common;
+
+namespace TutorProject.Searcher.Web.Controllers
+{
+    [Controller]
+    public class HomeController : Controller
+    {
+        private readonly TutorContext _tutorContext;
+        
+        public HomeController(TutorContext tutorContext)
+        {
+            _tutorContext = tutorContext;
+        }
+        
+        [HttpGet("/hello")]
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+    }
+}

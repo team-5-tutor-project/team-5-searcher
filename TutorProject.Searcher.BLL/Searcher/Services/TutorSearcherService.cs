23a4d7597c7459d6dfc238d08cdf3ecff502fdf3
ann.komova <ann.komova@gmail.com>
2022-06-09 00:35:22 +0300

    SRC-18: скрытые клиентом репетиторы не отображаются при поиске

--- TutorProject.Searcher.BLL/Searcher/Services/TutorSearcherService.cs
+++ TutorProject.Searcher.BLL/Searcher/Services/TutorSearcherService.cs
@@ -1,5 +1,6 @@
 using TutorProject.Account.Common;
 using TutorProject.Account.Common.Models;
+using TutorProject.Searcher.BLL.Blacklist.Repositories;
 using TutorProject.Searcher.BLL.Searcher.Repositories;
 
 namespace TutorProject.Searcher.BLL.Searcher.Services;
@@ -7,20 +8,51 @@ namespace TutorProject.Searcher.BLL.Searcher.Services;
 public class TutorSearcherService : ITutorSearcherService
 {
     private readonly TutorSearcherRepository _repository;
+    private readonly BlacklistRepository _blacklistRepository;
 
     public TutorSearcherService(TutorContext context)
     {
         _repository = new TutorSearcherRepository(context);
+        _blacklistRepository = new BlacklistRepository(context);
     }
 
-    public async Task<List<Tutor>> GetAll()
+    public async Task<List<Tutor>> GetAll(Guid clientId)
     {
-        return await _repository.GetAll();
+        var tutors = await _repository.GetAll();
+        var black = await  _blacklistRepository.GetTutorsFromBlacklist(clientId);
+        foreach (var clientTutor in black)
+        {
+            if (tutors.Contains(clientTutor.Tutor))
+            {
+                tutors.Remove(clientTutor.Tutor);
+            }
+        }
+        return tutors;
+    }
+
+    private async Task<bool> CheckInBlacklist(Guid clientId, Guid tutorId)
+    {
+        var tutorBlacklist = await _repository.CheckInBlacklist(clientId, tutorId);
+        if (tutorBlacklist == null)
+        {
+            return false;
+        }
+        return true;
     }
     
-    public async Task<List<TutorToSubject>> Search(string? subject, WorkFormat? workFormat, int? minPrice,
+    public async Task<List<Tutor>> Search(Guid clientId, string? subject, WorkFormat? workFormat, int? minPrice,
         int? maxPrice, int? pupilClass)
     {
-        return await _repository.Search(subject, workFormat, minPrice, maxPrice, pupilClass);
+        var tutorsToSubject = await _repository.Search( subject, workFormat, minPrice, maxPrice, pupilClass);
+        var tutors = new List<Tutor>();
+        foreach (var tutorToSubj in tutorsToSubject)
+        {
+            if (!await CheckInBlacklist(clientId, tutorToSubj.Tutor.Id) &&
+                !tutors.Contains(tutorToSubj.Tutor))
+            {
+                tutors.Add(tutorToSubj.Tutor);
+            }
+        }
+        return tutors;
     }
 }

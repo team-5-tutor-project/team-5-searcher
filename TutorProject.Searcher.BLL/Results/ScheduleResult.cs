555706975fa2210164813d2ca995dba445f6314c
teterina_m_o <teterina_m_o@rambler.ru>
2022-06-13 18:08:16 +0300

    add Results

---
+++ TutorProject.Searcher.BLL/Results/ScheduleResult.cs
@@ -0,0 +1,11 @@
+﻿using TutorProject.Account.Common.Models;
+
+namespace TutorProject.Searcher.BLL.Results;
+
+public class ScheduleResult
+{
+    public Tutor Tutor { get; init; }
+
+    public List<Day> FreeTimeSchedule { get; init; } = new List<Day>();
+}
+

e61e631e50c5875e69b735832cb8abaa71cd6db9
tatia2501 <catatat@yandex.ru>
2022-06-17 17:56:43 +0300

    SRC: добавлен выбор параметров для поиска

--- Front/TutorProject.Searcher.Front/TutorProject.Searcher.Front/Models/TutorToSearch.cs
+++ Front/TutorProject.Searcher.Front/TutorProject.Searcher.Front/Models/TutorToSearch.cs
@@ -2,14 +2,18 @@
 
 public class TutorToSearch
 {
-    public Guid TutorId;
     public string Subject { get; set; }
-    //public WorkFormat WorkFormat { get; set; }
     public string WorkFormat { get; set; }
     public int MinPrice { get; set; }
     public int MaxPrice { get; set; }
     public int PupilClass { get; set; }
-    //public TutorsOrder TutorsOrder { get; set; }
     public string TutorsOrder { get; set; }
-    public List<bool> Schedule { get; set; } 
+    //public List<bool> Schedule { get; set; }
+
+    public TutorToSearch()
+    {
+        Subject = "";
+        WorkFormat = "";
+        TutorsOrder = "";
+    }
 }

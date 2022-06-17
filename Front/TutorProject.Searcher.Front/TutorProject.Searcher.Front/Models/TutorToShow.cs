d7eca092cb43a5bb133f53383eb84ee5ddb2d07a
tatia2501 <catatat@yandex.ru>
2022-06-17 19:37:54 +0300

    SRC: изменен формат вывода найденных репетиторов

---
+++ Front/TutorProject.Searcher.Front/TutorProject.Searcher.Front/Models/TutorToShow.cs
@@ -0,0 +1,19 @@
+﻿namespace TutorProject.Searcher.Front.Models;
+
+public class TutorToShow
+{
+    public TutorResult Tutor { get; set; }
+    public string Subjects { get; set; }
+
+    public TutorToShow(TutorResult tutor, List<string> subjects)
+    {
+        Tutor = tutor;
+        Subjects = "";
+        foreach (var subject in subjects)
+        {
+            Subjects += subject + " ";
+        }
+
+        //Subjects.TrimEnd(',');
+    }
+}

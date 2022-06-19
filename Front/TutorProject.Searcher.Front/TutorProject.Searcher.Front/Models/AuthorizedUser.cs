7290d9e20a100f9f4d40b4a6c346d12add4f1720
ann.komova <ann.komova@gmail.com>
2022-06-19 22:39:25 +0300

    SRC-45: модель авторизованного юзера

--- Front/TutorProject.Searcher.Front/TutorProject.Searcher.Front/Models/AuthorizedUser.cs
+++ Front/TutorProject.Searcher.Front/TutorProject.Searcher.Front/Models/AuthorizedUser.cs
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-namespace TutorProject.Searcher.Front.Models;
-=======
 ﻿namespace TutorProject.Searcher.Front.Models;
->>>>>>> main
 
 public class AuthorizedUser
 {
     public Guid? UserId { get; set; }
-<<<<<<< HEAD
-        
-=======
->>>>>>> main
     public string? UserType { get; set; }
 }

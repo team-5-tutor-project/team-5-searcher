be01f6734fdacf78ca85ff9a5258b9da1b393f8f
teterina_m_o <teterina_m_o@rambler.ru>
2022-06-26 20:27:27 +0300

    исправление

--- Front/TutorProject.Searcher.Front/TutorProject.Searcher.Front/Program.cs
+++ Front/TutorProject.Searcher.Front/TutorProject.Searcher.Front/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Syncfusion.Blazor;
 using TutorProject.Searcher.Front;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");

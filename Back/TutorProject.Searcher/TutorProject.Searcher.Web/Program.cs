51e02ea510af1085aa76e98c53ea366556de7255
Mikhail <62665587+kawwik@users.noreply.github.com>
2022-06-27 01:33:10 +0300

    HOTFIX сделал сваггер доступным из прод окружения

--- Back/TutorProject.Searcher/TutorProject.Searcher.Web/Program.cs
+++ Back/TutorProject.Searcher/TutorProject.Searcher.Web/Program.cs
@@ -20,12 +20,9 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthorization();
 
